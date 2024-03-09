1d3f3412d01f89148e0974ee4132b4e2ccda15ae
beehoonsoup <baichi.belz@gmail.com>
2024-03-09 23:37:33 +0800

    update db

--- config/db.js
+++ config/db.js
@@ -2,9 +2,9 @@ const mysql = require('mysql2');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'mabelin',
-  password: '123',
-  database: 'sys',
+  user: 'root',
+  password: 'w2}JHOAH25MZ~8G',
+  database: 'tw2024',
   connectionLimit: 10
 });
 
